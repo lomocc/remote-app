c265fae33cf4c8e6abd44ff54b1f4eb3b6e70958
Vincent <vincent@rockx.com>
2022-09-16 19:38:40 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="inline-block text-3xl font-bold p-4 m-4 border rounded-full">
+    Hello{' '}
+    <a
+      className="text-blue-600 hover:underline"
+      href="https://webpack.js.org/concepts/module-federation/"
+    >
+      Module Federation
+    </a>{' '}
+    Remote App!
+  </div>
+);
 
 export default App;
